9f645a6ed7b6bf951b9317ba1183d1e08186db57
Robert Palmer <Awriter247@gmail.com>
2025-10-30 12:37:47 -0400

    initial commit

---
+++ SkillSnap_API_Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillSnap_API_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ SkillSnap_Client_Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace SkillSnap_Client_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
